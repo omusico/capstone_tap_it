f4e93df7cd919e66ddb0ba07626215d73afa0499
Stephen Lee <stlee78@u.washington.edu>
2015-03-12 13:09:44 -0700

    Added UI on waitline

--- BusinessApp/app/src/main/java/tapit/businessapp/TransmittingActivity.java
+++ BusinessApp/app/src/main/java/tapit/businessapp/TransmittingActivity.java
@@ -1,93 +1,93 @@
-package tapit.businessapp;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-
-import org.altbeacon.beacon.BeaconManager;
-
-
-public class TransmittingActivity extends Activity {
-    protected static final String TAG = "TransmittingActivity";
-    private BeaconManager beaconManager;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_transmitting);
-        verifyBluetooth();
-        logToDisplay("Application just launched" );
-    }
-
-    public void onRangingClicked(View view) {
-        Intent myIntent = new Intent(this, RangingActivity.class);
-        this.startActivity(myIntent);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        ((BeaconReferenceApplication) this.getApplicationContext()).setMonitoringActivity(this);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        ((BeaconReferenceApplication) this.getApplicationContext()).setMonitoringActivity(null);
-    }
-
-    private void verifyBluetooth() {
-
-        try {
-            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
-                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Bluetooth not enabled");
-                builder.setMessage("Please enable bluetooth in settings and restart this application.");
-                builder.setPositiveButton(android.R.string.ok, null);
-                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-                    @Override
-                    public void onDismiss(DialogInterface dialog) {
-                        finish();
-                        System.exit(0);
-                    }
-                });
-                builder.show();
-            }
-        }
-        catch (RuntimeException e) {
-            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Bluetooth LE not available");
-            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
-            builder.setPositiveButton(android.R.string.ok, null);
-            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-
-                @Override
-                public void onDismiss(DialogInterface dialog) {
-                    finish();
-                    System.exit(0);
-                }
-
-            });
-            builder.show();
-
-        }
-
-    }
-
-    public void logToDisplay(final String line) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-                EditText editText = (EditText)TransmittingActivity.this
-                        .findViewById(R.id.monitoringText);
-                editText.append(line+"\n");
-            }
-        });
-    }
-}
+//package tapit.businessapp;
+//
+//import android.app.Activity;
+//import android.app.AlertDialog;
+//import android.content.DialogInterface;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.View;
+//import android.widget.EditText;
+//
+//import org.altbeacon.beacon.BeaconManager;
+//
+//
+//public class TransmittingActivity extends Activity {
+//    protected static final String TAG = "TransmittingActivity";
+//    private BeaconManager beaconManager;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        Log.d(TAG, "onCreate");
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_transmitting);
+//        verifyBluetooth();
+//        logToDisplay("Application just launched" );
+//    }
+//
+//    public void onRangingClicked(View view) {
+//        Intent myIntent = new Intent(this, RangingActivity.class);
+//        this.startActivity(myIntent);
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        ((BeaconReferenceApplication) this.getApplicationContext()).setMonitoringActivity(this);
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        ((BeaconReferenceApplication) this.getApplicationContext()).setMonitoringActivity(null);
+//    }
+//
+//    private void verifyBluetooth() {
+//
+//        try {
+//            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+//                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                builder.setTitle("Bluetooth not enabled");
+//                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+//                builder.setPositiveButton(android.R.string.ok, null);
+//                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+//                    @Override
+//                    public void onDismiss(DialogInterface dialog) {
+//                        finish();
+//                        System.exit(0);
+//                    }
+//                });
+//                builder.show();
+//            }
+//        }
+//        catch (RuntimeException e) {
+//            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setTitle("Bluetooth LE not available");
+//            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+//            builder.setPositiveButton(android.R.string.ok, null);
+//            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+//
+//                @Override
+//                public void onDismiss(DialogInterface dialog) {
+//                    finish();
+//                    System.exit(0);
+//                }
+//
+//            });
+//            builder.show();
+//
+//        }
+//
+//    }
+//
+//    public void logToDisplay(final String line) {
+//        runOnUiThread(new Runnable() {
+//            public void run() {
+//                EditText editText = (EditText)TransmittingActivity.this
+//                        .findViewById(R.id.monitoringText);
+//                editText.append(line+"\n");
+//            }
+//        });
+//    }
+//}
