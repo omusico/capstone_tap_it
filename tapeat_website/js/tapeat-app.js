eece15f90c76b4037f7e3184662ff371c390892f
Frank Xue <frankx@porch.com>
2015-06-01 23:55:17 -0700

    integrated with stephen's work.

--- tapeat_website/js/tapeat-app.js
+++ tapeat_website/js/tapeat-app.js
@@ -1,3 +1,37 @@
+function genreateReservationCard (reservations) {
+      var waiterCard = $('#tapeat-reservation-card-for-waiter');
+      waiterCard.html('');
+      for (var reservation in reservations){
+        // Card Components
+        var cardComponent = '<div class="col s12 m6 waitline-card"> <div class="card white"> <div class="card-content"> <div class="left-panel col s2 m2 center-align"> <img id="profile_picture"class="profile"/> <p>Party Size: <span>2</span></p> </div> <div class="right-panel col s10 m10"> <div class="row"> <div class="col s12 m12"> <p id="customer_name">Customer</p> </div> </div> <div class="card-action"> <div class="col s3 m3"> <a data-option="confirm"><i class="custome mdi-navigation-check"></i></a> </div> <div class="col s3 m3"><a data-option="text"><i class="custome mdi-action-question-answer"></i></a> </div> <div class="col s3 m3"><a data-option="call"><i class="custome mdi-notification-phone-in-talk"></i></a> </div> <div class="col s3 m3"><a data-option="cancel"><i class="custome mdi-navigation-close"></i></a></div> </div> </div> </div>';
+        var customerCard = $(cardComponent);
+        var specificReservation = reservations[reservation];
+        customerCard.first().attr('id', reservation);
+        customerCard.find('#profile_picture').attr('data-name',specificReservation.customerName);
+        customerCard.find('#customer_name').html(specificReservation.customerName);
+        customerCard.find("[data-option='call']").attr("href", "tel:+" + specificReservation.customerPhone);
+        waiterCard.append(customerCard);
+      };
+      $('.profile').initial();
+}
+
+function genreateReservationTable(reservations){
+	var tableValue = '<tbody>';
+	var index = 1;
+	$.each(reservations, function(key, reservation){
+		tableValue += '<tr>';
+		tableValue += '<td>' + index + '</td>';
+		tableValue += '<td>' + reservation.customerName + '</td>';
+		tableValue += '<td>' + reservation.partySize + '</td>';
+		tableValue += '</tr>';
+		index++;
+	});
+	tableValue += '</tbody>';
+	$("#tapeat-reservation-table tbody").replaceWith(tableValue);
+}
+
+
+
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -27,52 +61,50 @@ function afterUserFirstTimeLogin(isRestaurant, authData){
 	userRef.update(userData);
 }
 
-
-function showTable(tableValue){
-	$("#index-banner").hide();
-	$("#tapeat-intro").hide();
-	$("#tapeat-content").hide();
-	$("#tapeat-contact").hide();
-	$("#tapeat-reservation-table tbody").replaceWith(tableValue);
-	$("#tapeat-reservation-table").show();
-}
-
-function hideTable(){
-	$("#index-banner").show();
-	$("#tapeat-intro").show();
-	$("#tapeat-content").show();
-	$("#tapeat-contact").show();
-	$("#tapeat-reservation-table tbody").replaceWith("<tbody></tbody>");
+function hideAll(){
+	$("#tapeat-intro-section").hide();
+	$("#tapeat-reservation-card-for-waiter").hide();
 	$("#tapeat-reservation-table").hide();
 }
 
 
-function showReservationWithoutModify(restaurantId, turnOff){
+function showTable(){
+	$("#tapeat-reservation-table").show();
+}
+
+function showWaiterCard(){
+	$("#tapeat-reservation-card-for-waiter").show();
+}
+
+function showIntro(){
+	$("#tapeat-intro-section").show();
+}
+
+
+function displayReservations(restaurantId, userRoleValue, turnOff){
 	//TODO: delete this after change all id to facebook buttonId
 	restaurantId = "dtfSeattleUniversityVillage1"
+
 	var restaurantReservation = ref.child("reservations/" + restaurantId);
 
 	if(turnOff){
-		restaurantReservation.off('value', hideTable());
+		hideAll();
+		restaurantReservation.off('value', showIntro());
 		return;
 	}
-
-	if(restaurantId){
-		restaurantReservation.on('value', function(snapshot) {
-			var tableValue = '<tbody>';
-			var index = 0;
-			$.each(snapshot.val(), function(key, reservation){
-				index++;
-				tableValue += '<tr>';
-				tableValue += '<td>' + index + '</td>';
-				tableValue += '<td>' + reservation.customerName + '</td>';
-				tableValue += '<td>' + reservation.partySize + '</td>';
-				tableValue += '</tr>';
-			});
-			tableValue += '</tbody>';
-			showTable(tableValue);
-		});
-	}
+	restaurantReservation.on('value', function(snapshot) {
+		hideAll();
+		var reservations = snapshot.val();
+		if(userRoleValue === "restaurant"){
+			genreateReservationCard(reservations);
+			showWaiterCard();
+		}else if(userRoleValue === "customer"){
+			genreateReservationTable(reservations);
+			showTable();
+		}else{
+			showIntro();
+		}
+	});
 }
 
 
@@ -100,20 +132,18 @@ function authDataCallback(authData) {
 
   	var userRole = ref.child("users" + "/" + authData.facebook.id + "/userRole");
 	userRole.once("value", function(snap) {
-	  if(snap.val()){
-	  	var userRoleValue = snap.val();
-	  	if(userRoleValue === "restaurant"){
-  			showReservationWithoutModify(authData.facebook.id, false);
-	  	}
-	  }else{
-	  	afterUserFirstTimeLogin(isRestaurant, authData);
-	  }
+		var userRoleValue = snap.val();
+		  if(userRoleValue){
+		  	displayReservations(authData.facebook.id, userRoleValue, false);
+		  }else{
+		  	afterUserFirstTimeLogin(isRestaurant, authData);
+		  }
 	});
 
   } else {
   	loginLink.show();
 
-  	showReservationWithoutModify(null, true);
+  	displayReservations(null, null, true);
 
   	var isRestaurant = getParameterByName("isRestaurant");
   	$(".login-link").on("click", function(){
